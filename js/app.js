053a196c10077b58070a91c6a870ce42bef4e015
David Chaves <davi.ch.fe@gmail.com>
2020-12-11 16:17:59 -0300

    Filtros

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ angular.module("listaTelefonica", []);
 angular.module("listaTelefonica").controller("ListaTelefonicaController", ($scope)=>{
     $scope.title = "Lista telefônica";
     $scope.contatos = [
-        {nome : "David", telefone : "99760-9338", cor : "blue" },
-        {nome : "Fulano", telefone : "99760-9338", cor : "yellow"},
-        {nome : "Ciclano", telefone : "99760-9338", cor : "red" }
+        {nome : "David", telefone : "99760-9338", cor : "blue", data : new Date(), op : { nome : "Tim",  codigo : 41, categoria : "celular"}},
+        {nome : "Fulano", telefone : "99760-9338", cor : "yellow", data : new Date(), op : { nome : "Oi",   codigo : 17, categoria : "celular"}},
+        {nome : "Ciclano", telefone : "99760-9338", cor : "red", data : new Date(), op : { nome : "GNV",  codigo : 21, categoria : "Telefone"}}
     ];
 
     $scope.operadoras = [
@@ -15,12 +15,18 @@ angular.module("listaTelefonica").controller("ListaTelefonicaController", ($scop
     ];
 
     $scope.add = (contato) => {
+        contato.data = new Date();
         $scope.contatos.push(contato);
+
         delete $scope.contato;
         $scope.contatosForm.$setPristine();
     }
 
     $scope.apagar = (contato) => {
         $scope.contatos = contato.filter(c => !c.selecionado);
+    }
+    $scope.ordenarPor = (ordenacao) => {
+        $scope.criterio = ordenacao;
+        $scope.ordem = !$scope.ordem;  
     }
 })
